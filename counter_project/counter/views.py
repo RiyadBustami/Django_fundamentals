513881f0cdf7add16002ef7ff06e49e6ce073f8c
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-09-21 10:17:52 +0300

    initial commit

---
+++ counter_project/counter/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def index(request):
+    if "counter" not in request.session:
+        request.session["counter"]=0
+        request.session.save()
+    request.session["counter"]+=1
+    request.session.save()
+    
+    return render(request,"index.html")
+
+def reset_session(request):
+    request.session["counter"]=0
+    request.session.save()
+    return redirect('/')
